08b618eb5d8a1c05049dcdae9a865af0f7529226
Julian Knodt <julianknodt@gmail.com>
2025-04-03 09:53:47 -0700

    Initial commit

---
+++ src/bin/texture_to_vert_colors.rs
@@ -0,0 +1,177 @@
+use clap::Parser;
+use pars3d::FaceKind;
+use pars3d::image::{self, DynamicImage, GenericImageView};
+use std::collections::BTreeMap;
+
+type F = f32;
+
+#[derive(Debug, Clone, PartialEq, Parser)]
+pub struct Args {
+    #[arg(long, short)]
+    input: String,
+    #[arg(long, short)]
+    output: String,
+}
+
+pub fn main() {
+    let args = Args::parse();
+    let scene = pars3d::load(args.input).expect("Failed to parse input");
+    let mut out_scene = scene.clone();
+    for (mi, mesh) in scene.meshes.iter().enumerate() {
+        assert!(!mesh.uv[0].is_empty());
+        let new_mesh = texture_to_vert_colors(mesh, &scene);
+        out_scene.meshes[mi] = new_mesh;
+    }
+    pars3d::save(args.output, &scene).expect("Failed to save output");
+}
+
+pub fn texture_to_vert_colors(mesh: &pars3d::Mesh, scene: &pars3d::Scene) -> pars3d::Mesh {
+    let mut out = pars3d::Mesh::new_geometry(mesh.v.clone(), vec![]);
+    out.n = mesh.n.clone();
+
+    // Copy vertex colors for each input UV
+    // TODO remove this assumption of a single material and copy per face with duplication
+    let mati = mesh
+        .single_mat()
+        .expect("More than 1 material for this mesh");
+    let mat = &scene.materials[mati];
+    let diff_tex = mat
+        .textures_by_kind(pars3d::mesh::TextureKind::Diffuse)
+        .next()
+        .expect("No diffuse texture?");
+    let diff_img = diff_tex.image.as_ref().expect("No diffuse image?");
+    for &[u, v] in &mesh.uv[0] {
+        let rgb = image::imageops::sample_bilinear(diff_img, u % 1., v % 1.).unwrap();
+        let [r, g, b, _a] = rgb.0.map(|c| c as F / 255.);
+        out.vert_colors.push([r, g, b]);
+    }
+
+    for (fi, f) in mesh.f.iter().enumerate() {
+        let Some(mati) = mesh.mat_for_face(fi) else {
+            continue;
+        };
+        let mat = &scene.materials[mati];
+        let Some(diff_tex) = mat
+            .textures_by_kind(pars3d::mesh::TextureKind::Diffuse)
+            .next()
+        else {
+            // TODO handle other kinds of textures as well.
+            continue;
+        };
+        let Some(ref diff_img) = diff_tex.image else {
+            continue;
+        };
+
+        sample(
+            &mesh,
+            f,
+            diff_img,
+            &mut out.f,
+            &mut out.v,
+            &mut out.vert_colors,
+        );
+    }
+    out
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct AABB<T, const N: usize> {
+    min: [T; N],
+    max: [T; N],
+}
+
+impl<const N: usize> AABB<F, N> {
+    pub fn new() -> Self {
+        Self {
+            min: [F::INFINITY; N],
+            max: [F::NEG_INFINITY; N],
+        }
+    }
+    pub fn add_point(&mut self, p: [F; N]) {
+        for i in 0..N {
+            self.min[i] = self.min[i].min(p[i]);
+            self.max[i] = self.max[i].max(p[i]);
+        }
+    }
+    pub fn round_to_i32(&self) -> AABB<i32, N> {
+        AABB {
+            min: self.min.map(|i| i.floor() as i32),
+            max: self.max.map(|i| i.ceil() as i32),
+        }
+    }
+    pub fn scale_by(&mut self, x: F, y: F) {
+        self.min[0] *= x;
+        self.max[0] *= x;
+
+        self.min[1] *= y;
+        self.max[1] *= y;
+    }
+}
+
+impl AABB<i32, 2> {
+    pub fn iter_coords(&self) -> impl Iterator<Item = [i32; 2]> + '_ {
+        let [lx, ly] = self.min;
+        let [hx, hy] = self.max;
+        (ly..=hy).flat_map(move |y| (lx..=hx).map(move |x| [x, y]))
+    }
+}
+
+pub fn sample(
+    mesh: &pars3d::Mesh,
+    f: &FaceKind,
+    diff_img: &DynamicImage,
+    out_faces: &mut Vec<FaceKind>,
+    out_verts: &mut Vec<[F; 3]>,
+    out_colors: &mut Vec<[F; 3]>,
+) {
+    let mut aabb = AABB::<F, 2>::new();
+    for uv in f.as_slice().iter().map(|&vi| mesh.uv[0][vi]) {
+        aabb.add_point(uv);
+    }
+    // TODO should these have -1?
+    let (w, h) = diff_img.dimensions();
+    aabb.scale_by(w as F, h as F);
+    let iaabb = aabb.round_to_i32();
+    let uv_f = f.map_kind(|v| mesh.uv[0][v]);
+    let v_f = f.map_kind(|v| mesh.v[v]);
+    let mut vert_map = BTreeMap::new();
+    for c in iaabb.iter_coords() {
+        let cf = c.map(|v| v as F);
+        let bary = uv_f.barycentric(cf);
+        if !bary.iter().all(|v| (0.0..=1.0).contains(v)) {
+            // outside the triangle
+            continue;
+        }
+        let new_vert = v_f.from_barycentric(bary);
+        // compute color
+        let rgba = image::imageops::interpolate_bilinear(diff_img, cf[0] % 1., cf[1] % 1.).unwrap();
+        let [r, g, b, _a] = rgba.0.map(|c| c as F / 255.);
+
+        let vi = out_verts.len();
+        out_verts.push(new_vert);
+        out_colors.push([r, g, b]);
+        assert!(vert_map.insert(c, vi).is_none());
+    }
+
+    // compute faces for each new vertex
+    for (&[u, v], &vi) in vert_map.iter() {
+        // TODO determine if this is +1 or -1?
+        let up = vert_map.get(u, v + 1);
+        let left = vert_map.get(u + 1, v);
+        let upleft = vert_map.get(u + 1, v + 1);
+        let new_face = match (up, upleft, left) {
+            (None, None, None) => {
+                /* TODO should form triangle with corners? */
+                continue;
+            }
+            (Some(u), Some(ul), Some(l)) => FaceKind::Quad([vi, u, ul, l]),
+            // All triples are a triangle
+            (Some(u), None, Some(l)) => FaceKind::Tri([vi, u, l]),
+            (Some(u), Some(ul), None) => FaceKind::Tri([vi, u, ul]),
+            (None, Some(ul), Some(l)) => FaceKind::Tri([vi, ul, l]),
+            // All doubles are steep triangles along the edge (handled elsewhere)
+            (Some(u), None, None) | (None, Some(ul), None) | (None, None, Some(l)) => continue,
+        };
+        out_faces.push(new_face);
+    }
+}

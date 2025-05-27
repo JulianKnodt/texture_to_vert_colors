bfc1c4807419c53b68bcfb9cd8dee68ddddb7831
Julian Knodt <julianknodt@gmail.com>
2025-05-27 18:29:33 +0900

    Update dithering binary with color

--- src/bin/dithering.rs
+++ src/bin/dithering.rs
@@ -1,8 +1,10 @@
 #![allow(unused)]
 use clap::Parser;
 use ordered_float::NotNan;
-use texture_to_vert_colors::{F, add, dist, kmul, sub};
+use texture_to_vert_colors::weighting::{PosColorNorm, WeightingKind};
+use texture_to_vert_colors::{F, add, dist, dot, kmul, sub};
 
+use pars3d::adjacency::VertexAdj;
 use priority_queue::PriorityQueue;
 
 #[derive(Debug, Clone, PartialEq, Parser)]
@@ -16,37 +18,81 @@ pub struct Args {
     output: String,
 
     /// How to weigh which vertices get how much diffusion
-    #[arg(long, short, default_value_t = WeightKind::Uniform)]
-    weight_kind: WeightKind,
-    // error diffusion kind (uniform, laplacian, distance)
+    #[arg(long, short, default_value_t = WeightingKind::Uniform)]
+    weighting: WeightingKind,
 
-    // use a different palette
+    /// How to evaluate color and position
+    #[arg(long, default_value_t = PosColorNorm::PosOnly)]
+    pos_color_norm: PosColorNorm,
 
     #[arg(long, default_value_t = OrderKind::Nearest)]
     order: OrderKind,
+
+    // use a different palette
+    /// Use RGB instead of grayscale.
+    #[arg(long)]
+    rgb: bool,
+
+    /// Use a different palette for dithering (Default = [0., 1.])
+    #[arg(long, short)]
+    palette: Vec<F>,
 }
 
 fn main() {
-    let args = Args::parse();
+    let mut args = Args::parse();
+    if args.palette.is_empty() {
+        args.palette = vec![0., 1.];
+    }
+    args.palette.sort_unstable_by(F::total_cmp);
+    let args = args;
     let start = std::time::Instant::now();
     let scene =
         pars3d::load(&args.input).expect(&format!("Failed to parse input from {}", args.input));
     let mut m = scene.into_flattened_mesh();
     assert_eq!(m.vert_colors.len(), m.v.len());
+    let og_f = m.f.clone();
+    m.triangulate();
     let (s, t) = m.normalize();
 
-    let mut vert_grayscale = m
-        .vert_colors
-        .iter()
-        .map(|[r, g, b]| (r + g + b) / 3.)
-        .collect::<Vec<_>>();
+    let vv_adj = args
+        .weighting
+        .vertex_weights(&m, args.pos_color_norm)
+        .expect("Failed to construct weighting");
 
-    let mut barycentric_areas = vec![];
-    pars3d::geom_processing::barycentric_areas(&m.f, &m.v, &mut barycentric_areas);
-    // normalize barycentric areas?
+    if args.rgb {
+        for c in 0..3 {
+            let mut color_chan = m.vert_colors.iter().map(|rgb| rgb[c]).collect::<Vec<_>>();
+            dither(&vv_adj, &mut color_chan, &args.palette, &args);
 
-    let palette = [0., 1.];
+            for (i, &g) in color_chan.iter().enumerate() {
+                m.vert_colors[i][c] = g;
+            }
+        }
+    } else {
+        let lum_chan = [0.2126, 0.7152, 0.0722];
+        //let lum_chan = [0.299, 0.587, 0.114];
+        let mut vert_grayscale = m
+            .vert_colors
+            .iter()
+            .map(|&rgb| dot(rgb, lum_chan))
+            .collect::<Vec<_>>();
 
+        dither(&vv_adj, &mut vert_grayscale, &args.palette, &args);
+
+        for (i, &g) in vert_grayscale.iter().enumerate() {
+            assert!((0.0..=1.0).contains(&g), "{g}");
+            m.vert_colors[i] = [g; 3];
+        }
+    }
+
+    m.denormalize(s, t);
+    m.f = og_f;
+    let s = m.into_scene();
+    pars3d::save(&args.output, &s).expect("Failed to save output");
+    println!("[INFO]: Took {:?} for visualization", start.elapsed());
+}
+
+pub fn dither(vv_adj: &VertexAdj<F>, channel: &mut [F], palette: &[F], args: &Args) {
     // enqueue each vertex into a queue, and take the vertex which has the least difference with
     // quantized result and convert it to quantized value
     let mut pq: PriorityQueue<usize, _> = PriorityQueue::new();
@@ -57,86 +103,64 @@ fn main() {
             palette
                 .iter()
                 //.map(|&p| (p, dist(p, c)))
-                .map(|&p| (p, (p-c).abs()))
+                .map(|&p| (p, (p - c).abs()))
                 .min_by(|(_, a), (_, b)| a.partial_cmp(&b).unwrap())
                 .unwrap()
         }};
     }
 
-    for (vi, &vc) in vert_grayscale.iter().enumerate() {
+    for (vi, &vc) in channel.iter().enumerate() {
         let (_, dist) = nearest_palette_and_cost!(vc);
 
         let p = match args.order {
-          OrderKind::Nearest => NotNan::new(-dist).unwrap(),
-          OrderKind::Random => NotNan::new(0.).unwrap(),
-          OrderKind::Farthest => NotNan::new(dist).unwrap(),
+            OrderKind::Nearest => NotNan::new(-dist).unwrap(),
+            OrderKind::Random => NotNan::new(0.).unwrap(),
+            OrderKind::Farthest => NotNan::new(dist).unwrap(),
         };
         pq.push(vi, p);
     }
 
-    let vv_adj = m.vertex_vertex_adj();
-
     while let Some((vi, _)) = pq.pop() {
-        let curr_color = vert_grayscale[vi];
+        let curr_color = channel[vi];
         let nearest = nearest_palette_and_cost!(curr_color).0;
 
         // RGB error to be diffused to other vertices
         let err = nearest - curr_color;
-        vert_grayscale[vi] = nearest;
+        channel[vi] = nearest;
 
-        let adj_verts = vv_adj.adj(vi);
         let mut total_w = 0.;
-        for &adj_vi in adj_verts {
+        for (adj_vi, w) in vv_adj.adj_data(vi) {
             let adj_vi = adj_vi as usize;
             // do not push errors on to vertices which have already been quantized (?)
             // maybe there is some version where this can be recursively applied
-            if palette.contains(&vert_grayscale[adj_vi]) {
+            if palette.contains(&channel[adj_vi]) {
                 continue;
             }
 
             // uniform for now
-            total_w += match args.weight_kind {
-                WeightKind::Uniform => 1.,
-                WeightKind::BarycentricArea => barycentric_areas[adj_vi],
-                WeightKind::Laplacian => todo!(),
-            };
+            total_w += w;
         }
         if total_w <= 0. {
             continue;
         }
-        for &adj_vi in adj_verts {
+        for (adj_vi, w) in vv_adj.adj_data(vi) {
             let adj_vi = adj_vi as usize;
-            if palette.contains(&vert_grayscale[adj_vi]) {
+            if palette.contains(&channel[adj_vi]) {
                 continue;
             }
-            let w = match args.weight_kind {
-                WeightKind::Uniform => 1.,
-                WeightKind::BarycentricArea => barycentric_areas[adj_vi],
-                WeightKind::Laplacian => todo!(),
-            };
             // uniform for now
             let frac = w / total_w;
-            vert_grayscale[adj_vi] -= frac * err;
-            let (_, dist) = nearest_palette_and_cost!(vert_grayscale[adj_vi]);
+            channel[adj_vi] -= frac * err;
+            let (_, dist) = nearest_palette_and_cost!(channel[adj_vi]);
             let p = match args.order {
-              OrderKind::Nearest => NotNan::new(-dist).unwrap(),
-              OrderKind::Random => NotNan::new(0.).unwrap(),
-              OrderKind::Farthest => NotNan::new(dist).unwrap(),
+                OrderKind::Nearest => NotNan::new(-dist).unwrap(),
+                OrderKind::Random => NotNan::new(0.).unwrap(),
+                OrderKind::Farthest => NotNan::new(dist).unwrap(),
             };
             let prev = pq.change_priority(&adj_vi, p);
             assert_ne!(prev, None);
         }
     }
-
-    for (i, &g) in vert_grayscale.iter().enumerate() {
-      assert!((0.0..=1.0).contains(&g), "{g}");
-      m.vert_colors[i] = [g; 3];
-    }
-
-    m.denormalize(s, t);
-    let s = m.into_scene();
-    pars3d::save(&args.output, &s).expect("Failed to save output");
-    println!("[INFO]: Took {:?} for visualization", start.elapsed());
 }
 
 macro_rules! impl_display {
@@ -152,20 +176,6 @@ macro_rules! impl_display {
     }
   }
 }
-
-#[derive(Debug, Clone, Copy, PartialEq, clap::ValueEnum)]
-pub enum WeightKind {
-    Uniform,
-    BarycentricArea,
-    Laplacian,
-}
-
-impl_display!(
-  WeightKind,
-  Uniform => "uniform",
-  BarycentricArea => "barycentric-area",
-  Laplacian => "laplacian",
-);
 
 #[derive(Debug, Clone, Copy, PartialEq, clap::ValueEnum)]
 pub enum OrderKind {

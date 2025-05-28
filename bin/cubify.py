c2fb1cd1396b3cf6eb8d2e04d186df26221741de
Julian Knodt <julianknodt@gmail.com>
2025-05-28 20:42:48 +0900

    Test more related to cubifying texture edge

--- bin/cubify.py
+++ bin/cubify.py
@@ -1,6 +1,6 @@
 import trimesh
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
-from tqdm import trange
+from tqdm import trange, tqdm
 
 import torch.nn.functional as F
 import torch.optim as optim
@@ -38,7 +38,7 @@ def arguments():
     help="Optimize colors as well as vertex positions (TODO this needs to be figured out more)",
   )
   a.add_argument(
-    "--iters", type=int, default=50,
+    "--iters", type=int, default=75,
     help="Iterations to run of cubification",
   )
   a.add_argument(
@@ -50,11 +50,13 @@ def arguments():
 def main():
   args = arguments()
   mesh = trimesh.load_mesh(args.input)
+
   #TODO here need to do mesh normalization
   device = args.device
   if not torch.cuda.is_available() and device == "cuda":
     print("[WARN]: CUDA is not available, defaulting to CPU")
     device = "cpu"
+
   v = torch.from_numpy(mesh.vertices).float().to(device)
   og_v = v.clone().detach()
   v.requires_grad_(True)
@@ -64,49 +66,103 @@ def main():
 
   vc = None
   if args.color_cubeness > 0:
+    assert(hasattr(mesh.visual, "vertex_colors")), "Input mesh does not have vertex colors"
     # TODO here emit an error if this does not have vertex colors
     vc = torch.from_numpy(mesh.visual.vertex_colors).div(255).to(device)[:, :3]
     vc.requires_grad_(True)
     assert(vc.shape == v.shape)
 
   L, M = robust_laplacian.mesh_laplacian(np.array(mesh.vertices), np.array(mesh.faces))
+  L_map = {}
+  for ci in range(V):
+    for ind in range(L.indptr[ci], L.indptr[ci+1]):
+        row = int(L.indices[ind])
+        data = L.data[ind]
+        L_map[(row, ci)] = data
 
   print("[INFO]: Starting precomputation...")
-  # for each vertex, construct neighborhood edges (either spokes or spokes-or-rims)
+  # for each vertex, construct neighborhood edges of spokes-and-rims
   nbr_edges = [set() for _ in range(V)]
   bary_area = torch.zeros(V,requires_grad=False, device=device)
 
   area_weighted_normal = torch.zeros_like(v, requires_grad=False)
+  luma_prim_dir = torch.zeros_like(v, requires_grad=False)
 
   all_edges = set()
-
   with torch.no_grad():
-    for v0, v1, v2 in mesh.faces:
-      v0, v1, v2 = [int(vi) for vi in [v0, v1, v2]]
+    # discrete set of edges
+    for vi0, vi1, vi2 in tqdm(mesh.faces, leave=False):
+      vi0, vi1, vi2 = [int(vi) for vi in [vi0, vi1, vi2]]
 
-      edge_set = [(v0, v1), (v1, v2), (v2, v0)]
-      nbr_edges[v0].update(edge_set)
-      nbr_edges[v1].update(edge_set)
-      nbr_edges[v2].update(edge_set)
-      all_edges.update(edge_set)
-      normal = torch.cross(v[v1] - v[v0], v[v2] - v[v0], dim=-1)
-      area = normal.norm() / 2.
-      for vi in [v0, v1, v2]:
+      edge_set = [(vi0, vi1), (vi1, vi2), (vi2, vi0)]
+      nbr_edges[vi0].update(edge_set)
+      nbr_edges[vi1].update(edge_set)
+      nbr_edges[vi2].update(edge_set)
+      all_edges.update(
+        [(min(e0, e1), max(e0, e1)) for (e0, e1) in edge_set]
+      )
+
+    # Tensors for each edge set
+    vis = [[], [], []]
+    for vi_ in mesh.faces:
+      for i in range(3):
+        vis[i].append(int(vi_[i]))
+
+    vi0, vi1, vi2 = [torch.tensor(vi, device=device) for vi in vis]
+    v0 = v[vi0]
+    v1 = v[vi1]
+    v2 = v[vi2]
+
+    if vc is not None:
+      l0 = luma(vc[vi0])
+      l1 = luma(vc[vi1])
+      l2 = luma(vc[vi2])
+
+      normals = torch.cross(v1 - v0, v2 - v0, dim=-1)
+      area = normals.norm(dim=-1) / 2.
+      for vi in [vi0, vi1, vi2]:
         bary_area[vi] += area / 3.
-        area_weighted_normal[vi] += F.normalize(normal, dim=0) * area
+        area_weighted_normal[vi] += F.normalize(normals, dim=-1) * area[:, None]
 
-    area_weighted_normal = F.normalize(area_weighted_normal, dim=-1)
+      area_weighted_normal = F.normalize(area_weighted_normal, dim=-1)
 
-  # for each vertex, store a tensor of N(i) x 2 (first vertex, second vertex)
 
+      # given area weighted normals, compute linear functional G for luma of each point?
+      P = torch.stack([
+        append_val(v0),
+        append_val(v1),
+        append_val(v2),
+        append_val(F.normalize(normals, dim=-1), one=True),
+      ], dim=-2)
+      S = torch.stack([
+        l0, l1, l2, torch.zeros_like(l0)
+      ], dim=-1)
+      # TODO in theory could manually implement the inversion here
+      GD = torch.linalg.lstsq(P, S, rcond=1e-5, driver="gelsd").solution
+
+      assert(GD.isfinite().all())
+      G = GD[:, :3]
+
+      # store G into each vertex
+      for vi in [vi0, vi1, vi2]:
+        # TODO normalize G[vi]?
+        luma_prim_dir[vi] += G[vi] * area[:, None]
+
+  #ei0, ei1 = [
+  #  torch.tensor([e[i] for e in all_edges])
+  #  for i in [0,1]
+  #]
+  #luma_delta = (luma(vc[ei0]) - luma(vc[ei1])).abs()
+
+
+  print("[INFO]: Starting Laplacian computation")
   laplacians = []
-  for es in nbr_edges:
+  for es in tqdm(nbr_edges, leave=False):
     lapl_diag = []
     for e0, e1 in es:
-      assert(L[e0, e1] == L[e1, e0])
-      lapl_diag.append(-L[e0, e1])
+      lapl_diag.append(-L_map.get((e0, e1), 0))
+      #lapl_diag.append(-L[(e0, e1)]) # do not index directly, otherwise it will be slow
     laplacians.append(torch.tensor(lapl_diag, dtype=torch.float, device=device))
-
 
   print("[INFO]: Constructing acceleration structures...")
   max_degree = max(len(nes) for nes in nbr_edges) + 1
@@ -132,18 +188,6 @@ def main():
   nbr_eis = [ torch.tensor(nbr_ei, device=device) for nbr_ei in nbr_eis if len(nbr_ei) > 0 ]
   nbr_ls  = [ torch.stack(nbr_ls) for nbr_ls in nbr_ls if len(nbr_ls) > 0 ]
 
-  all_es = torch.stack([
-      torch.tensor([ae[i] for ae in all_edges], device=device) for i in [0,1]
-  ], dim=-1)
-  all_e0 = all_es[:, 0]
-  all_e1 = all_es[:, 1]
-
-  color_grad = 0
-  if vc is not None:
-    color_grad = (vc[all_e1] - vc[all_e0]).norm(dim=-1)
-  print(all_es.shape)
-  exit()
-
   # DONE COMPUTING ACCELERATION ---
 
   params=[v]
@@ -164,7 +208,6 @@ def main():
   rot_opt = optim.Adam(params=[quats], lr=3e-3)
   for i in t_outer:
     # START QUAT OPTIMIZATION --------
-    #manually renormalize
 
     vd = v.clone().detach().requires_grad_(False)
     t = trange(25, leave=False)
@@ -177,18 +220,29 @@ def main():
         idxs = nbr_eis[i]
         ei0, ei1 = [ei.squeeze(-1) for ei in nbr_ets[i].split([1,1], dim=-1)]
         R = mats[idxs]
+
         D = og_v[ei1] - og_v[ei0]
         Dp = vd[ei1] - vd[ei0]
+        # extra weighting term
+        #Li = (luma(vc[ei1]) - luma(vc[ei0])).abs()
+        #Li = (1. - Li)[..., None]
+
         z = (R @ D.mT) - Dp.mT
 
         Ls = nbr_ls[i][:, None]
+        arap = 0.5 * torch.vmap(torch.trace)((z * Ls) @ z.mT)
+
         A = bary_area[idxs]
         ni = area_weighted_normal[idxs, :, None]
+        cubeness = 0
+        if args.cubeness > 0:
+          cubeness = args.cubeness * A * (R @ ni).abs().squeeze(dim=-1).sum(dim=-1)
 
-        arap = 0.5 * torch.vmap(torch.trace)((z * Ls) @ z.mT)
-        cubeness = args.cubeness * A * (R @ ni).abs().squeeze(dim=-1).sum(dim=-1)
-        #tex_cubeness =
         loss = loss + (arap + cubeness).sum()
+      t.set_postfix(L=f"{loss.item():.3f}")
+      loss.backward()
+      rot_opt.step()
+      quats.data = F.normalize(quats.data, dim=-1)
 
       ## flat version kept for posterity
       #for vi in range(V):
@@ -205,11 +259,6 @@ def main():
       #  arap = 0.5 * torch.trace((z * laplacians[vi][None]) @ z.T)
       #  cubeness = args.cubeness * bary_area[vi] * (R @ area_weighted_normal[vi]).abs().sum()
       #  loss = loss + arap + cubeness
-
-      t.set_postfix(L=f"{loss.item():.3f}")
-      loss.backward()
-      rot_opt.step()
-      quats.data = F.normalize(quats.data, dim=-1)
 
     mats = quat_to_mat(quats).detach().requires_grad_(False)
     # END QUAT OPTIMIZATION ----------
@@ -242,11 +291,15 @@ def main():
       z = (R @ D.mT) - Dp.mT
 
       Ls = nbr_ls[i][:, None]
-      A = bary_area[idxs]
-      ni = area_weighted_normal[idxs, :, None]
 
       arap = 0.5 * torch.vmap(torch.trace)((z * Ls) @ z.mT)
-      cubeness = args.cubeness * A * (R @ ni).abs().squeeze(dim=-1).sum(dim=-1)
+
+      A = bary_area[idxs]
+      ni = area_weighted_normal[idxs, :, None]
+      cubeness = 0
+      if args.cubeness > 0:
+        cubeness = args.cubeness * A * (R @ ni).abs().squeeze(dim=-1).sum(dim=-1)
+
       loss = loss + (arap + cubeness).sum()
 
     t_outer.set_postfix(L=f"{loss.item():.3f}")
@@ -277,5 +330,16 @@ def quat_to_mat(q, dim=-1):
 #print(quat_to_mat(torch.tensor([t.tolist(), [0., 0., 0., 1.]])))
 #exit()
 
+def luma(rgb, dim=-1,keepdim=False):
+  r,g,b = rgb.split([1,1,1], dim=dim)
+  l = 0.299 * r + 0.587 * g + 0.114 * b
+  if keepdim: return l
+  return l.squeeze(dim=dim)
+
+def append_val(v, one:bool=False, dim=-1):
+  s = list(v.shape)
+  s[dim] = 1
+  func = torch.zeros if one else torch.ones
+  return torch.cat([v, func(s, device=v.device, dtype=v.dtype)], dim=dim)
 
 if __name__ == "__main__": main()

8e8a5a35054a5c27127317115de66f2540b160ca
Julian Knodt <julianknodt@gmail.com>
2025-06-24 20:19:18 +0900

    Add bin for creating a colorbar

---
+++ bin/colorbar.py
@@ -0,0 +1,25 @@
+#generate a colorbar
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import argparse
+
+a = argparse.ArgumentParser()
+a.add_argument("--units", required=True, help="Units to display")
+a.add_argument("--max", default=1, type=float, help="Max unit")
+a.add_argument("--min", default=0, type=float, help="Min unit")
+a.add_argument("--vertical", action="store_true")
+args = a.parse_args()
+
+mpl.rcParams.update({'font.size': 12})
+
+fig, ax = plt.subplots(figsize=(1, 8) if args.vertical else (6, 1), layout='constrained')
+
+cmap = mpl.cm.magma
+norm = mpl.colors.Normalize(vmin=args.min, vmax=args.max)
+
+fig.colorbar(mpl.cm.ScalarMappable(norm=norm, cmap=cmap),
+             cax=ax,
+             orientation='vertical' if args.vertical else 'horizontal',
+             label=args.units)
+
+plt.savefig("colorbar.pdf", bbox_inches="tight")
